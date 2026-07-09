fb188de653bd34968635184ef81ed3bef65198df
ai-prompt <ai-prompt@guijishuhui.com>
2026-07-09 14:26:17 +0800

    更新邀请码-6个新码(1年有效)

--- invite-codes-20260709.js
+++ invite-codes-20260709.js
@@ -1,31 +1,13 @@
 // 邀请码本地验证数据
-// 生成时间: 2026-07-09
-// 格式: { hash: SHA-256(邀请码), expires: 到期时间, used: false }
+// 自动生成于: 2026-07-09
+// 新码有效期：1年（2027-07-09到期）
 // CloudBase 不可用时的备选方案
 
 window.INVITE_CODES = [
-        "hash": "796f20d5bf78b1debfea51be6f573ab09a884c97c7614dfb17b8bf43b78666ba",
-        "expires": "2026-07-14 15:39:45",
-        "used": false
-    },
-    {
-        "hash": "5816542fca2469b90531925c34a09d523116e230faa7a379aeed89920e71357c",
-        "expires": "2026-07-14 15:39:45",
-        "used": false
-    },
-    {
-        "hash": "da044f14a295b62604871f4f70316fc1eb66faa4b95c6cdba9fa2e055f013893",
-        "expires": "2026-07-14 15:39:45",
-        "used": false
-    },
-    {
-        "hash": "43e428ca6c885547ac56f42f60aa7ff9b51fc4ecbe0969e39f150cd71fbe84b6",
-        "expires": "2026-07-14 15:39:46",
-        "used": false
-    },
-    {
-        "hash": "f1c63a7ee562e13fce20fa8ef62bf0e6d93526ff69173138d8a0f35b2a2e864c",
-        "expires": "2026-07-14 15:39:46",
-        "used": false
-    }
+    {"hash":"b088bf24515dbb3b5e4a4cff74375218b660d86a6d07b798cbfa2d5c7e5ae088","expires":"2027-07-09 14:24:16","used":false},
+    {"hash":"83c340fc0f8cd8fe572d60b91668b1b46405b008e462fb6417a573227908de77","expires":"2027-07-09 14:24:17","used":false},
+    {"hash":"1757cbaeb20a763a99bd1932aaf845bbc0b3c47e45a03b0ad96569c0fbd8d625","expires":"2027-07-09 14:24:19","used":false},
+    {"hash":"2ff66d9eab72e81c4bbc3fa7e7c3b59968b025fc0632d7cbbeff7bc1b02116c7","expires":"2027-07-09 14:24:21","used":false},
+    {"hash":"5823a13c7ea4ab9f2fe496be30be36e3df949c749c74d9661ef4e5b2bcb50fe8","expires":"2027-07-09 14:24:22","used":false},
+    {"hash":"aa09d4c03b12ecee62926638eb9bdc2588e518340bef58815eb3ce5acac7ff24","expires":"2027-07-09 14:24:24","used":false}
 ];
