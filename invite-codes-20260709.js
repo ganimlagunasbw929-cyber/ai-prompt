0c5ad519134d775c8aa754c44840abdb25813e0c
ganimlagunasbw929-cyber <ganimlagunasbw929-cyber@users.noreply.github.com>
2026-07-10 09:06:20 +0800

    修复吸顶间距、邀请码验证、生成新邀请码

--- invite-codes-20260709.js
+++ invite-codes-20260709.js
@@ -1,13 +1,30 @@
-// 邀请码本地验证数据
-// 自动生成于: 2026-07-09
-// 新码有效期：1年（2027-07-09到期）
-// CloudBase 不可用时的备选方案
+// 邀请码本地验证数据（4组 x 5个 = 20个）
+// 自动生成于: 2026-07-09 17:46:52
+// 有效期至: 2027-07-09 17:46:52（1年）
 
 window.INVITE_CODES = [
-    {"hash":"b088bf24515dbb3b5e4a4cff74375218b660d86a6d07b798cbfa2d5c7e5ae088","expires":"2027-07-09 14:24:16","used":false},
-    {"hash":"83c340fc0f8cd8fe572d60b91668b1b46405b008e462fb6417a573227908de77","expires":"2027-07-09 14:24:17","used":false},
-    {"hash":"1757cbaeb20a763a99bd1932aaf845bbc0b3c47e45a03b0ad96569c0fbd8d625","expires":"2027-07-09 14:24:19","used":false},
-    {"hash":"2ff66d9eab72e81c4bbc3fa7e7c3b59968b025fc0632d7cbbeff7bc1b02116c7","expires":"2027-07-09 14:24:21","used":false},
-    {"hash":"5823a13c7ea4ab9f2fe496be30be36e3df949c749c74d9661ef4e5b2bcb50fe8","expires":"2027-07-09 14:24:22","used":false},
-    {"hash":"aa09d4c03b12ecee62926638eb9bdc2588e518340bef58815eb3ce5acac7ff24","expires":"2027-07-09 14:24:24","used":false}
+  // ========== 第A组 ==========
+  {"hash":"209eb8d4e388ebd6b7b284eb6015db62afbadbf889357bcc3a2627afbdfda36b","expires":"2027-07-09 17:46:52","used":false},  // GPT2-WWAD-4011
+  {"hash":"d34819c8f7b956bb2054a7ad88da08eb1869e2d32137af583fea01afec849478","expires":"2027-07-09 17:46:52","used":false},  // GPT2-372C-3082
+  {"hash":"5cb2f468966bb7521aa67166edb70e22db8970b4b74763f12da4d322a6addcdd","expires":"2027-07-09 17:46:52","used":false},  // GPT2-PZ0G-8127
+  {"hash":"5cd6ec1695854391e26a36d3e291142cb789374aa9b3bf2e3f9736a58b0e3f9b","expires":"2027-07-09 17:46:52","used":false},  // GPT2-FAP8-7037
+  {"hash":"ae7c3ea4d34b3e842a0fd329a1834f90bb833cd7793fc87eff5172ac6f0ca62c","expires":"2027-07-09 17:46:52","used":false},  // GPT2-F1B6-9668
+  // ========== 第B组 ==========
+  {"hash":"d876f44805db357c1cccf873d821890c15b008f8ac3d9c0a1d4296d2bff33b36","expires":"2027-07-09 17:46:52","used":false},  // GPT2-ZO9Q-8090
+  {"hash":"b2d0ccb3b67a85938dd50625563e33b2f2aaf50485f11181cfa4f449d1db2e93","expires":"2027-07-09 17:46:52","used":false},  // GPT2-F0DX-2268
+  {"hash":"188153902e97f66acf497d829bf8d959263db5b26852bc3267aa7d4db7d83cad","expires":"2027-07-09 17:46:52","used":false},  // GPT2-TUCJ-1071
+  {"hash":"67c5b0396cad5bddf74328847733f15da82b159317f1408e340cd565f3410931","expires":"2027-07-09 17:46:52","used":false},  // GPT2-L7WM-5666
+  {"hash":"18fdd78a6333d5efb34f9c122a72c67f67deb1e84354190aa895ec22ca7752b5","expires":"2027-07-09 17:46:52","used":false},  // GPT2-052O-5376
+  // ========== 第C组 ==========
+  {"hash":"5515ddbd7cd8a3695f8739a1d46b982ae507dc2e8724f683ebc1c7dff68d13c1","expires":"2027-07-09 17:46:52","used":false},  // GPT2-0B1R-2888
+  {"hash":"3202d235515982e845bd3d8c7293c04c304585aee06f16cd34ef770448d2da28","expires":"2027-07-09 17:46:52","used":false},  // GPT2-C829-5879
+  {"hash":"fc9e58ec9ca101eb3e586d03e9910a816236d430943aa8fb79a5231b085f6c31","expires":"2027-07-09 17:46:52","used":false},  // GPT2-NYRV-2801
+  {"hash":"e74250c60447a8bbf6ea3c0829ed7c1731a99eb93481efa27f7cbee900e4e41a","expires":"2027-07-09 17:46:52","used":false},  // GPT2-Q83D-6708
+  {"hash":"58396f1c1b4f6d7c14684bf6fd5521555bbe284ebd1ef59cdda4c092b4474090","expires":"2027-07-09 17:46:52","used":false},  // GPT2-10DY-5565
+  // ========== 第D组 ==========
+  {"hash":"18eb2a0891e80556c789ae228c67b74a02a5dfa4bd8dd266d87cb13c7b0a8742","expires":"2027-07-09 17:46:52","used":false},  // GPT2-QB8I-4830
+  {"hash":"3c4635959382a891e834fddc8d215040487753cd9e4fc850da2aa9803f664553","expires":"2027-07-09 17:46:52","used":false},  // GPT2-EKMP-2664
+  {"hash":"89e10a9bd278f46c35b05329a1e322ba5d0033daa5e9811a74209333f159c346","expires":"2027-07-09 17:46:52","used":false},  // GPT2-5G6I-0044
+  {"hash":"c99f95bb533252df0d50d4dd94dc101e44f4e1bb39b5a6cb069bf0ffd4cafd15","expires":"2027-07-09 17:46:52","used":false},  // GPT2-FD72-9452
+  {"hash":"38436e576c3d9f85dccfa1848cdd611e90763a53fe509d4b24d74a197d3923cb","expires":"2027-07-09 17:46:52","used":false},  // GPT2-MWQH-0933
 ];
